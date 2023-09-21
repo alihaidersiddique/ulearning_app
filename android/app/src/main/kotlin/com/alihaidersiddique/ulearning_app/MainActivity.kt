ac5cbb1c0d23af029ca194da8e4bf3480bd450a4
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2023-09-21 20:31:41 +0500

    counter implemented with bloc

---
+++ android/app/src/main/kotlin/com/alihaidersiddique/ulearning_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alihaidersiddique.ulearning_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/app_events.dart';
+import 'package:ulearning_app/app_states.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AppBlocs(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Counter',
+        theme: ThemeData(useMaterial3: true),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Counter Home Page")),
+        body: BlocBuilder<AppBlocs, AppStates>(
+          builder: (context, state) => Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  "${BlocProvider.of<AppBlocs>(context).state.counter}",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: () =>
+                  BlocProvider.of<AppBlocs>(context).add(Increment()),
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: () =>
+                  BlocProvider.of<AppBlocs>(context).add(Decrement()),
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
